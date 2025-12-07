6e8a5a82ff155bcd2958687fb10f7c9891c58f47
Amine Abbouti <amine.abbouti@hotmail.com>
2025-12-07 21:50:52 +0100

    feat: Add ProgressView component with circular and linear variants

---
+++ apps/mobile/modules/sykamore-ui/ios/ProgressView.swift
@@ -0,0 +1,28 @@
+import ExpoModulesCore
+import SwiftUI
+
+internal enum ProgressVariant: String, Enumerable {
+  case circular
+  case linear
+}
+
+final class ProgressProps: UIBaseViewProps {
+  @Field var variant: ProgressVariant = .circular
+  @Field var progress: Double?
+  @Field var color: Color?
+}
+
+struct ProgressView: ExpoSwiftUI.View {
+  @ObservedObject var props: ProgressProps
+
+  var body: some View {
+    SwiftUI.ProgressView(value: props.progress)
+      .tint(props.color)
+      .if(props.variant == .circular) {
+        $0.progressViewStyle(.circular)
+      }
+      .if(props.variant == .linear) {
+        $0.progressViewStyle(.linear)
+      }
+  }
+}
