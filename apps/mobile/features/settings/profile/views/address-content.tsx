51167261dda181977e9e8a44f1c8bfd70af846c4
Amine Abbouti <amine.abbouti@hotmail.com>
2026-01-17 00:38:29 +0100

    feat: enhance AddressContent layout with Section wrapper and improve input field structure

--- apps/mobile/features/settings/profile/views/address-content.tsx
+++ apps/mobile/features/settings/profile/views/address-content.tsx
@@ -6,6 +6,8 @@ import {
   LabeledContent,
   accessibilityLabel,
   frame,
+  Section,
+  Text,
 } from '@/modules/sykamore-ui';
 import {useTheme} from '@/providers';
 import {useProfile} from '../hooks';
@@ -46,90 +48,99 @@ export const AddressContent = forwardRef<AddressContentHandle>((_, ref) => {
   return (
     <Host style={{flex: 1}}>
       <Form>
-        <LabeledContent label="Street">
-          <TextField
-            placeholder="Street"
-            defaultValue={address}
-            textContentType="street-address-line1"
-            keyboardType="default"
-            submitLabel="done"
-            autocapitalization="words"
-            onChangeText={setAddressValue}
-            numberOfLines={1}
-            autoFocus
-            modifiers={[
-              accessibilityLabel('Street input field'),
-              frame({maxWidth: ds.screen.width / 2}),
-            ]}
-          />
-        </LabeledContent>
+        <Section
+          footer={
+            <Text>
+              Address can be changed here, it will not be visible to other users
+              within your organization except for the ones with admin or manager
+              permissions.
+            </Text>
+          }
+        >
+          <LabeledContent label="Street">
+            <TextField
+              placeholder="Street"
+              defaultValue={address}
+              textContentType="street-address-line1"
+              keyboardType="default"
+              submitLabel="done"
+              autocapitalization="words"
+              onChangeText={setAddressValue}
+              numberOfLines={1}
+              modifiers={[
+                accessibilityLabel('Street input field'),
+                frame({maxWidth: ds.screen.width / 1.8}),
+              ]}
+            />
+          </LabeledContent>
 
-        <LabeledContent label="City">
-          <TextField
-            placeholder="City"
-            defaultValue={city}
-            textContentType="address-city"
-            keyboardType="default"
-            submitLabel="done"
-            autocapitalization="words"
-            onChangeText={setCityValue}
-            numberOfLines={1}
-            modifiers={[
-              accessibilityLabel('City input field'),
-              frame({maxWidth: ds.screen.width / 2}),
-            ]}
-          />
-        </LabeledContent>
+          <LabeledContent label="City">
+            <TextField
+              placeholder="City"
+              defaultValue={city}
+              textContentType="address-city"
+              keyboardType="default"
+              submitLabel="done"
+              autocapitalization="words"
+              onChangeText={setCityValue}
+              numberOfLines={1}
+              modifiers={[
+                accessibilityLabel('City input field'),
+                frame({maxWidth: ds.screen.width / 1.8}),
+              ]}
+            />
+          </LabeledContent>
 
-        <LabeledContent label="Province">
-          <TextField
-            placeholder="Province"
-            defaultValue={province}
-            textContentType="address-state"
-            keyboardType="default"
-            submitLabel="done"
-            autocapitalization="words"
-            onChangeText={setProvinceValue}
-            numberOfLines={1}
-            modifiers={[
-              accessibilityLabel('Province input field'),
-              frame({maxWidth: ds.screen.width / 2}),
-            ]}
-          />
-        </LabeledContent>
+          <LabeledContent label="Province">
+            <TextField
+              placeholder="Province"
+              defaultValue={province}
+              textContentType="address-state"
+              keyboardType="default"
+              submitLabel="done"
+              autocapitalization="words"
+              onChangeText={setProvinceValue}
+              numberOfLines={1}
+              modifiers={[
+                accessibilityLabel('Province input field'),
+                frame({maxWidth: ds.screen.width / 1.8}),
+              ]}
+            />
+          </LabeledContent>
 
-        <LabeledContent label="Postal Code">
-          <TextField
-            placeholder="Postal Code"
-            defaultValue={postalCode}
-            textContentType="postal-code"
-            keyboardType="default"
-            submitLabel="done"
-            autocapitalization="characters"
-            onChangeText={setPostalCodeValue}
-            numberOfLines={1}
-            modifiers={[
-              accessibilityLabel('Postal code input field'),
-              frame({maxWidth: ds.screen.width / 2}),
-            ]}
-          />
-        </LabeledContent>
+          <LabeledContent label="Postal Code">
+            <TextField
+              placeholder="Postal Code"
+              defaultValue={postalCode}
+              textContentType="postal-code"
+              keyboardType="default"
+              submitLabel="done"
+              autocapitalization="characters"
+              onChangeText={setPostalCodeValue}
+              numberOfLines={1}
+              modifiers={[
+                accessibilityLabel('Postal code input field'),
+                frame({maxWidth: ds.screen.width / 1.8}),
+              ]}
+            />
+          </LabeledContent>
 
-        <LabeledContent label="Country">
-          <TextField
-            placeholder="Country"
-            defaultValue={country}
-            keyboardType="default"
-            submitLabel="done"
-            autocapitalization="words"
-            onChangeText={setCountryValue}
-            numberOfLines={1}
-            modifiers={[
-              accessibilityLabel('Country input field'),
-              frame({maxWidth: ds.screen.width / 2}),
-            ]}
-          />
-        </LabeledContent>
+          <LabeledContent label="Country">
+            <TextField
+              placeholder="Country"
+              defaultValue={country}
+              keyboardType="default"
+              submitLabel="done"
+              autocapitalization="words"
+              onChangeText={setCountryValue}
+              numberOfLines={1}
+              modifiers={[
+                accessibilityLabel('Country input field'),
+                frame({maxWidth: ds.screen.width / 1.8}),
+              ]}
+            />
+          </LabeledContent>
+        </Section>
       </Form>
     </Host>
   );
