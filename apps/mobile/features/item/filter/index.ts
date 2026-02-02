46cc7a952c9f222de6f4d21dede6c9cb2e569d82
Amine Abbouti <amine.abbouti@hotmail.com>
2026-02-02 23:33:40 +0100

    feat: remove item filter components and related hooks to streamline item filtering functionality

--- apps/mobile/features/item/filter/index.ts
+++
@@ -1,4 +0,0 @@
-// Item Filter Components Barrel File
-export {StatusFilter} from './status-filter';
-export {TrackingModeFilter} from './tracking-mode-filter';
-export {PriceRangeFilter} from './price-range-filter';
